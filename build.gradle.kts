2d8375e1acf70c3d98189c2cbe364b3d3d0ffb61
Surakuri Morimoto <Adriancarlcaerlang@gmail.com>
2026-07-09 14:24:34 +0800

    Testing native Kotlin bug hunters

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+// Add the formatter (Ktlint)
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+
+// Add the bug hunter (Detekt)
+    id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
